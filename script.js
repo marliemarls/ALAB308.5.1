4bcee37be6ab658d19d008742a835970a1894f0c
Marlie Martinez <marlieahue@gmail.com>
2024-05-17 14:54:33 -0400

    working on this still

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function returnSum(arr) {
     return sum
 };
 
-console.log(returnSum(arrayTest))
+// console.log(returnSum(arrayTest))
 
 //given an array ofnumbers, return the average of the sum
 function returnAverage(arr) {
@@ -25,7 +25,7 @@ function returnAverage(arr) {
     return sum
 };
 
-console.log(returnAverage(arrayTest))
+// console.log(returnAverage(arrayTest))
 
 //given an array of strings, return the longest string. using the sort method i can do this easily 
 function longestStr(arr){
@@ -36,5 +36,38 @@ function longestStr(arr){
     return arrayMap[0]
 }
 
-console.log(longestStr(arrayStrings))
+// console.log(longestStr(arrayStrings))
+
 //given an array of strings AND a number, return an array of the strings longer than the given number.
+//function will take in an array and a number
+//within the function there will be a newArray variable created that is an empty array
+//function will iterate through the array and look at the length of each element
+//if the element length is larger than the given number, it will push the element into newArray
+//else it will continue to iterate
+//in the end of the for loop, the function will return the newArray
+
+function largerThan(arr, num) {
+    let newArray = [];
+    for(let i = 0; i <= arr.length; i++){
+        const element = arr[i]
+        console.log(element)
+        
+    }
+
+}
+
+console.log(largerThan(arrayStrings, 3))
+
+
+//given a number (n), print every number between 1 & n without using loops. USE RECURSION.
+
+// function everyNumberBetween (num) {
+//     let count = 1
+//     if(count < num){
+//         console.log(count)
+//         count++
+//     }
+
+// }
+
+// console.log(everyNumberBetween(5))
